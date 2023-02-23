d65c1638140acb8ddea7d11776aabbc5e7102a21
“OdinsBeard82 <danielcharles_500@hotmail.com>
2023-02-23 09:09:06 +0000

    created grid of 16x16

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const container = document.getElementById("grid-container");
 
 for (let i = 0; i < 16; i++) {
-  const item = document.createElement("div");
-  item.classList.add("col");
-  container.appendChild(item);
-  
+  const row = document.createElement("div");
+  row.classList.add("row");
+  container.appendChild(row);
+
+  for (let j = 0; j < 16; j++) {
+    const col = document.createElement("div");
+    col.classList.add("col");
+    row.appendChild(col);
+  }
 }
