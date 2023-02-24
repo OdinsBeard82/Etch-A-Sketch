594b991221fa5edb396bb9459f8c41efa688a30b
“OdinsBeard82 <danielcharles_500@hotmail.com>
2023-02-24 11:39:27 +0000

    button that resets the game created

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const container = document.getElementById("grid-container");
 
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  container.appendChild(row);
+function createGrid(size) {
+  container.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
 
-  for (let j = 0; j < 16; j++) {
-    const col = document.createElement("div");
-    col.classList.add("col");
-    row.appendChild(col);
+    for (let j = 0; j < size; j++) {
+      const col = document.createElement("div");
+      col.classList.add("col");
+      row.appendChild(col);
 
-    col.addEventListener("mouseenter", () => {
-      col.classList.add("hover");
-    });
-
+      col.addEventListener("mouseenter", () => {
+        col.classList.add("hover");
+      });
+    }
   }
 }
+
+function resetGrid() {
+  createGrid(16);
+}
+
+createGrid(16);
+
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener("click", resetGrid);
