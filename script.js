c63e607c021a7facb182f07c162587c98bed6571
“OdinsBeard82 <danielcharles_500@hotmail.com>
2023-03-22 12:21:36 +0000

    Calculator works

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const answerDiv = document.querySelector('.answer');
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    answerDiv.textContent = button.value;
-  });
+button.addEventListener('click', () => {
+if (button.value === '=') {
+answerDiv.textContent = eval(answerDiv.textContent);
+} else if (button.value === 'AC') {
+answerDiv.textContent = '0';
+} else {
+if (answerDiv.textContent === '0') {
+answerDiv.textContent = '';
+}
+answerDiv.textContent += button.value;
+}
+});
 });
