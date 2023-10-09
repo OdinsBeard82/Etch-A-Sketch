d90638e123e6ffde0a1b1967da0b3a4c5702b18c
OdinsBeard82 <danielcharles_500@hotmail.com>
2023-10-09 13:57:14 +0100

    back to correct commit

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-const answerDiv = document.querySelector('.answer');
+const container = document.getElementById("grid-container");
+function createGrid(size) {
+  container.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let j = 0; j < size; j++) {
+      const col = document.createElement("div");
+      col.classList.add("col");
+      row.appendChild(col);
 
-const buttons = document.querySelectorAll('button');
+      col.addEventListener("mouseenter", () => {
+        col.classList.add("hover");
+        col.style.backgroundColor = getRandomColor();
+      });
+    }
+  }
+}
 
-buttons.forEach(button => {
-button.addEventListener('click', () => {
-if (button.value === '=') {
-answerDiv.textContent = eval(answerDiv.textContent);
-} else if (button.value === 'AC') {
-answerDiv.textContent = '0';
-} else {
-if (answerDiv.textContent === '0') {
-answerDiv.textContent = '';
+function resetGrid() {
+  let newSize = prompt("Enter new grid size");
+  if (newSize !== null && newSize !== '' && newSize <= 100) {
+    createGrid(parseInt(newSize));
+  }
 }
-answerDiv.textContent += button.value;
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
-});
-});
+
+createGrid(16);
+
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener("click", resetGrid);
+const refreshButton = document.querySelector(".refresh");
+
+const refreshPage = () => {
+  location.reload();
+}
+
+refreshButton.addEventListener("click", refreshPage)
+
